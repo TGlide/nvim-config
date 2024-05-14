b086359cc2dd0402114b204f72e166c4fe6a9620
tglide <26071571+TGlide@users.noreply.github.com>
2024-05-14 23:31:45 +0100

    small changes

--- lua/thomasgen/lazy/lualine.lua
+++ lua/thomasgen/lazy/lualine.lua
@@ -110,7 +110,7 @@ return {
                     "diff",
                     "diagnostics",
                 },
-                lualine_c = { "filename" },
+                lualine_c = { { "filename", path = 1 } },
                 lualine_x = { "filetype" },
                 lualine_y = { "progress" },
                 lualine_z = {
--- lua/thomasgen/lazy/telescope.lua
+++ lua/thomasgen/lazy/telescope.lua
@@ -8,7 +8,11 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            pickers = {
+                find_files = { hidden = true }
+            }
+        })
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
