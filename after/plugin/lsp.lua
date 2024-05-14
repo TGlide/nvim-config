b893f2b26837d55b833da3ebaaf9f21a6d9af8b2
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2024-05-14 11:04:46 +0100

    Configure Vue LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,44 +1,69 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 -- to learn how to use mason.nvim
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {
-	  "eslint",
-  },
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
+    ensure_installed = {
+        "eslint",
+        "tsserver"
+    },
+    handlers = {
+        function(server_name)
+            require('lspconfig')[server_name].setup({})
+        end,
+    },
 })
 
- local cmp = require('cmp')
- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+-- Setup specific LSPs
+local lua_opts = lsp_zero.nvim_lua_ls()
+require('lspconfig').lua_ls.setup(lua_opts)
 
- cmp.setup({
-	 snippet = {
-		 expand = function(args)
-			 require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-		 end,
-	 },
-	 mapping = cmp.mapping.preset.insert({
-		 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-		 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-		 ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-		 ["<C-Space>"] = cmp.mapping.complete(),
-	 }),
-	 sources = cmp.config.sources({
-		 { name = 'nvim_lsp' },
-		 { name = 'luasnip' }, -- For luasnip users.
-	 }, {
-		 { name = 'buffer' },
-	 })
- })
+local mason_registry = require('mason-registry')
+local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() ..
+    '/node_modules/@vue/language-server'
+
+require 'lspconfig'.tsserver.setup {
+  init_options = {
+    plugins = {
+      {
+        -- this happneed to be installed, maybe it does not need to be added manually but if it does not work then try to install it
+        name = '@vue/typescript-plugin',
+        location = vue_language_server_path,
+        languages = { 'vue' },
+      },
+    },
+  },
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+}
+require 'lspconfig'.volar.setup {}
+
+-- Keybindings
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, -- For luasnip users.
+    }, {
+        { name = 'buffer' },
+    })
+})
--- lua/thomasgen/lazy.lua
+++ lua/thomasgen/lazy.lua
@@ -12,25 +12,30 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+    -- Colors
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    { "rose-pine/neovim", name = "rose-pine" },
+    
+    -- File navigation
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.6',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
-    { "rose-pine/neovim", name = "rose-pine" },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
     'theprimeagen/harpoon',
-    'mbbill/undotree',
-    -- 'tpope/vim-fugitive',
 
-    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+    'mbbill/undotree',
+
+    -- LSP Zero
     {'williamboman/mason.nvim'},
     {'williamboman/mason-lspconfig.nvim'},
-
     {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
     {'neovim/nvim-lspconfig'},
     {'hrsh7th/cmp-nvim-lsp'},
     {'hrsh7th/nvim-cmp'},
     {'L3MON4D3/LuaSnip'},
+
+    -- Git
+    -- 'tpope/vim-fugitive',
     {
         "NeogitOrg/neogit",
         dependencies = {
