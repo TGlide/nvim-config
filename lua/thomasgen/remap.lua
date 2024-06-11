d340bc5b634d931f5d2acdbecc748b9941a46046
tglide <26071571+TGlide@users.noreply.github.com>
2024-06-11 17:31:12 +0100

    arrow key line movement

--- lua/thomasgen/remap.lua
+++ lua/thomasgen/remap.lua
@@ -4,7 +4,9 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Move lines around
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv")
 
 -- Keep cursor in center throughout some operations
 vim.keymap.set("n", "J", "mzJ`z")
