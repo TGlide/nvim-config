45d6b354945f7cfbb68757902f9c7974ae4cae50
tglide <26071571+TGlide@users.noreply.github.com>
2024-06-14 17:12:56 +0100

    fix linting

--- lua/thomasgen/lazy/linting.lua
+++ lua/thomasgen/lazy/linting.lua
@@ -1,30 +1,30 @@
 return {
-    "mfussenegger/nvim-lint",
-    event = {
-        "BufReadPre",
-        "BufNewFile",
-    },
-    config = function()
-        local lint = require("lint")
+	"mfussenegger/nvim-lint",
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
+	config = function()
+		local lint = require("lint")
 
-        lint.linters_by_ft = {
-            javascript = { "eslint" },
-            typescript = { "eslint" },
-            javascriptreact = { "eslint" },
-            typescriptreact = { "eslint" },
-            svelte = { "eslint" },
-            vue = { "eslint" },
-            python = { "pylint" },
-            astro = { "eslint" }
-        }
+		lint.linters_by_ft = {
+			javascript = { "eslint" },
+			typescript = { "eslint" },
+			javascriptreact = { "eslint" },
+			typescriptreact = { "eslint" },
+			svelte = { "eslint" },
+			vue = { "eslint" },
+			python = { "pylint" },
+			astro = { "eslint" }
+		}
 
-        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
-            group = lint_augroup,
-            callback = function()
-                lint.try_lint()
-            end,
-        })
-    end,
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint(nil, { ignore_errors = true })
+			end,
+		})
+	end,
 }
